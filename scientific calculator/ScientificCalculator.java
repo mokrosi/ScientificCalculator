85af65840fdcabcbdcff80501c25ff928112006d
krosi <krosidigital@gmail.com>
2025-07-20 11:01:48 +0400

    finsh main menu

--- scientific calculator/ScientificCalculator.java
+++ scientific calculator/ScientificCalculator.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class ScientificCalculator {
 
-    Scanner input = new Scanner(System.in);
+    public static Scanner input = new Scanner(System.in);
     public static void main (String [] args ){
-
+        menu();
         
 
 
@@ -23,13 +23,27 @@ public class ScientificCalculator {
             System.out.println("4.Division (/)");
             System.out.println("5.Square Root (√x)");
             System.out.println("6.Power (x^y)");
-            System.err.println("Sine (sin(x))");
-            System.out.println("Tangent(tan(x))");
-            System.out.println("Natural Logarithm (lnx)");
-            System.out.println("Logarithm Base 10 (log10x)");
-            System.out.println("");
+            System.err.println("7.Sine (sin(x))");
+            System.out.println("8.Tangent(tan(x))");
+            System.out.println("9.Natural Logarithm (lnx)");
+            System.out.println("10.Logarithm Base 10 (log10x)");
+            System.out.println("11.AbsoluteValue");
+            System.out.println("12.Round (to nearest long)");
+            System.out.println("13.Ceiling (round up)");
+            System.out.println("14.Floor (round down)");
+            System.out.println("14.Minimum of two numbers");
+            System.out.println("15.Maximum of two numbers");
+            System.out.println("16.Helper methods");
+            System.out.println("17.Exit");
+            System.out.print("chose Number : ");
+            int menuChose = input.nextInt();
 
-            return 0 ;
+            if (menuChose > 0 && menuChose <= 17){
+                loopSwitch = false;
+                return menuChose ;
+            }
+            System.out.println("no item fiend pleas chose from 1 to 17 ");
+
         }
 
         return 0;

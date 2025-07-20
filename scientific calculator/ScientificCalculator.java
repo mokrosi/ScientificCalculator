ab1c8c17dcc2223e253de247707a3ecb014e6b02
krosi <krosidigital@gmail.com>
2025-07-20 10:24:00 +0400

    firest push

---
+++ scientific calculator/ScientificCalculator.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ScientificCalculator {
+
+    Scanner input = new Scanner(System.in);
+    public static void main (String [] args ){
+
+        
+
+
+    }
+
+
+    public static int  menu() {
+        
+        boolean loopSwitch = true ;
+
+        while (loopSwitch) {
+
+            System.out.println("1.Addition (+)");
+            System.out.println("2.Subtraction (-)");
+            System.out.println("3.Multiplication (*)");
+            System.out.println("4.Division (/)");
+            System.out.println("5.Square Root (√x)");
+            System.out.println("6.Power (x^y)");
+            System.err.println("Sine (sin(x))");
+            System.out.println("Tangent(tan(x))");
+            System.out.println("Natural Logarithm (lnx)");
+            System.out.println("Logarithm Base 10 (log10x)");
+            System.out.println("");
+
+            return 0 ;
+        }
+
+        return 0;
+
+    }
+}
